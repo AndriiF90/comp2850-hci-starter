ddd7181ac7583c1ae2fd051726c1d92b361dbda2
Andrii <andriiforosiuk@gmail.com>
2025-12-12 02:27:10 +0000

    Update taskstore.kt

--- src/main/kotlin/storage/taskstore.kt
+++ src/main/kotlin/storage/taskstore.kt
@@ -1,1 +1,20 @@
+package storage
 
+import model.Task
+
+class TaskStore {
+    private val tasks = mutableListOf<Task>()
+
+    fun getAll(): List<Task> = tasks.toList()
+    fun getById(id: String): Task? = tasks.find { it.id == id }
+    fun add(task: Task) { tasks.add(task) }
+    fun update(task: Task): Boolean {
+        val index = tasks.indexOfFirst { it.id == task.id }
+        if (index != -1) {
+            tasks[index] = task
+            return true
+        }
+        return false
+    }
+    fun delete(id: String): Boolean = tasks.removeIf { it.id == id }
+}
